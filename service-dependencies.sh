712607fdd2e860fc2c77bc8cc8fc6f7ab5ee7133
Aravind Jarpala <aravind_jarpala@Aravinds-MacBook-Pro.local>
2024-04-21 10:26:53 +0530

    Added few condititons to check

--- service-dependencies.sh
+++ service-dependencies.sh
@@ -24,15 +24,14 @@ install_homebrew() {
 
 get_docker() {
     local os=$1
+    if ! command -v docker &> /dev/null; then
+        if [[ $os =~ [macMac]$ ]]; then
+            echo "Installing Docker using Homebrew..."
+            brew install --cask docker
 
-    if [[ $os =~ [macMac]$ ]]; then
-        echo "Installing Docker using Homebrew..."
-        # brew install --cask docker
-
-        # echo "Starting Docker..."
-        # open /Applications/Docker.app
-    else
-        if ! [ -d "/Applications/Docker.app" ]; then
+            echo "Starting Docker..."
+            open /Applications/Docker.app
+        else
             echo "Installing Docker..."
             curl -o $DOWNLOADS_FOLDER/Docker.dmg https://desktop.docker.com/mac/main/arm64/Docker.dmg
 
@@ -49,23 +48,24 @@ get_docker() {
                 rm Docker.dmg
             fi
             echo "✅ Docker installation complete."
-        else 
-            echo "Docker already installed"
         fi
+    else  
+        echo "Docker already installed"
     fi
 }
 
 get_teleport() {
     echo "Downloading teleport...."
     if ! command -v tsh &> /dev/null; then
-        # curl -o $DOWNLOADS_FOLDER/teleport-15.2.2.pkg https://cdn.teleport.dev/teleport-15.2.2.pkg
+        curl -o $DOWNLOADS_FOLDER/teleport-15.2.2.pkg https://cdn.teleport.dev/teleport-15.2.2.pkg
         
         echo "Downloaded the teleport package"
-        # sudo installer -pkg $DOWNLOADS_FOLDER/teleport-15.2.2.pkg -target /
+        sudo installer -pkg $DOWNLOADS_FOLDER/teleport-15.2.2.pkg -target /
         echo "✅ Teleport got installed"
     else
         echo "✅ Teleport is already installed"
     fi
+
     tsh version
 }
 
@@ -74,14 +74,14 @@ get_awscli() {
     if ! command -v aws &> /dev/null; then
         if [[ $os =~ [macMac]$ ]]; then
             echo "Downloading AWS CLI using brew....." 
-            # brew install awscli
+            brew install awscli
         
         else
             echo "Downloading AWS CLI..."
-            # curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+            curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 
             echo "Downloaded the AWS CLI package"
-            # sudo installer -pkg AWSCLIV2.pkg -target /
+            sudo installer -pkg AWSCLIV2.pkg -target /
         fi
         echo "Verifying AWS CLI installation..."
         aws --version
@@ -89,42 +89,56 @@ get_awscli() {
     else
         echo "AWS is already installed."
     fi
+
 }
 
 get_google_cloud_sdk() {
     local os=$1
     echo "Downloading Google Cloud SDK... "
-    if [[ $os =~ [macMac]$ ]]; then
-        echo "Downloading Google Cloud SDK using brew... "
-        # brew install --cask google-cloud-sdk
-    else
-        echo "Downloading Google Cloud SDK... "
-        # curl -o /$DOWNLOADS_FOLDER/google-cloud-cli-472.0.0-darwin-arm.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-472.0.0-darwin-arm.tar.gz
-
-        echo "Extracting Google Cloud SDK..."
-        # tar -xvf google-cloud-cli-472.0.0-darwin-arm.tar.gz
-
-        read -p "Navigate to google-cloud-sdk directory (y/N): " choice
-        if [[ $choice =~ ^[Yy]$ ]]; then
-            cd $DOWNLOADS_FOLDER/google-cloud-sdk
-
-            echo "Running installation script..."
-            # ./install.sh  
+    if ! command -v gcloud &> /dev/null; then
+        if [[ $os =~ [macMac]$ ]]; then
+            echo "Downloading Google Cloud SDK using brew... "
+            brew install --cask google-cloud-sdk
         else
-            echo "You can manually navigate to google-cloud-sdk directory and run the installation script."
+            echo "Downloading Google Cloud SDK... "
+            curl -o /$DOWNLOADS_FOLDER/google-cloud-cli-472.0.0-darwin-arm.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-472.0.0-darwin-arm.tar.gz
+
+            echo "Extracting Google Cloud SDK..."
+            tar -xvf google-cloud-cli-472.0.0-darwin-arm.tar.gz
+
+            read -p "Navigate to google-cloud-sdk directory (y/N): " choice
+            if [[ $choice =~ ^[Yy]$ ]]; then
+                cd $DOWNLOADS_FOLDER/google-cloud-sdk
+
+                echo "Running installation script..."
+                ./install.sh  
+            else
+                echo "You can manually navigate to google-cloud-sdk directory and run the installation script."
+            fi
         fi
+
+    else
+        echo "Google Cloud SDK is already installed"
     fi
+    read -p "Do you want to install kubectl & gke-gcloud-auth-plugin (y/N): " install_choice
 
-    echo "Installing Kubectl..."
-    # gcloud components install kubectl
+    if [[ $install_choice =~ [yY]$ ]]; then
+        echo "Installing Kubectl..."
+        gcloud components install kubectl
 
-    echo "Installing gke-gcloud-auth-plugin..."
-    # gcloud components install gke-gcloud-auth-plugin
+        echo "Installing gke-gcloud-auth-plugin..."
+        gcloud components install gke-gcloud-auth-plugin
 
+        echo "Verifying Kubectl & gke-gcloud installation..."
+        kubectl version --client
+        gke-gcloud-auth-plugin --version
+    else
+        echo "You can manually install those using 'gcloud components install kubectl / gke-gcloud-auth-plugin.'"
+
+    fi
     read -p "Authorize to Google Cloud SDK (y/N): " auth_choice
-
     if [[ $auth_choice =~ [Yy]$ ]]; then
-        # gcloud auth login
+        gcloud auth login
         echo
     else
         echo "SDK not authorized. You can manually authorize SDK later using 'gcloud auth login'."
@@ -132,21 +146,23 @@ get_google_cloud_sdk() {
 
     echo "Verifying Google Cloud SDK installation..."
     gcloud --version
-    kubectl version --client
-    gke-gcloud-auth-plugin --version
 }
 
 get_jdk() {
     local os=$1
     echo "Downloading .dmg jdk file......"
-    if [[ $os =~ [macMac]$ ]]; then
-        # brew install openjdk
+    if ! command -v java &> /dev/null; then
+        if [[ $os =~ [macMac]$ ]]; then
+            brew install openjdk
 
-        echo "Verifying JDK installation..."
-        java --version
+            echo "Verifying JDK installation..."
+            java --version
+        else
+            curl -o /$DOWNLOADS_FOLDER/jdk-22_macos-aarch64_bin.dmg https://download.oracle.com/java/22/latest/jdk-22_macos-aarch64_bin.dmg
+            echo "✅ JDK downloaded. From either the browser Downloads window or from the file browser, double-click the .dmg file to start it."
+        fi
     else
-        # curl -o /$DOWNLOADS_FOLDER/jdk-22_macos-aarch64_bin.dmg https://download.oracle.com/java/22/latest/jdk-22_macos-aarch64_bin.dmg
-        echo "✅ JDK downloaded. From either the browser Downloads window or from the file browser, double-click the .dmg file to start it."
+        echo "JDK is already installed"
     fi
 }
 
@@ -155,13 +171,13 @@ get_node() {
     if ! command -v node &> /dev/null; then
         if [[ $os =~ [macMac]$ ]]; then
             echo "Installing Node using brew..."
-            # brew install node@20
+            brew install node@20
         else
             echo "Installing Node..."
-            # sudo apt install nodejs
+            sudo apt install nodejs
 
             echo "Installing NPM..."
-            # sudo apt install npm 
+            sudo apt install npm 
         fi
 
         echo "Verifying the right Node.js version is in the environment"
@@ -180,6 +196,7 @@ get_all() {
     install_homebrew $os
     get_teleport
     get_jdk $os
+    get_awscli $os
     get_google_cloud_sdk $os
     get_docker $os
     get_node $os
@@ -189,9 +206,10 @@ main() {
     local cmd=$1
     local os=$2
     case "$cmd" in
-        "homebrew") install_homebrew "$os";;
-        "install") get_teleport ;;
+        "brew") install_homebrew "$os";;
+        "teleport") get_teleport ;;
         "jdk") get_jdk $os;;
+        "aws") get_awscli $os ;;
         "google-cloud-sdk") get_google_cloud_sdk $os;;
         "docker") get_docker $os;;
         "node") get_node "$os" ;;
